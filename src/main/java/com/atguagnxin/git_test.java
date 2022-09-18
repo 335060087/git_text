c30ccd46d84391fa9c043705cef9f25acba03f9b
GD <335060087@qq.com>
2022-09-18 22:03:10 +0800

    second

--- src/main/java/com/atguagnxin/git_test.java
+++ src/main/java/com/atguagnxin/git_test.java
@@ -3,7 +3,7 @@ package com.atguagnxin;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
 
     }
 }
