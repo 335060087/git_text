66ccaa14e39f291947dbe9cf28f6b5ecbb7b7d0e
GD <335060087@qq.com>
2022-09-18 22:18:31 +0800

    third

--- src/main/java/com/atguagnxin/git_test.java
+++ src/main/java/com/atguagnxin/git_test.java
@@ -4,6 +4,8 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello hot_fix");
+        System.out.println("hello hot_fix111");
 
     }
 }
